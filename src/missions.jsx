49b521122990bf825dba5704920d70c3406cc12f
Team <yehudarothstein@gmail.com>
2024-12-02 17:36:07 +0200

    Initial Commit

---
+++ src/missions.jsx
@@ -0,0 +1,185 @@
+export const missions = [
+    {
+        id: 1,
+        name: "Coral Nursery",
+        color: "olive",
+        points: [
+            {
+                description: "Place a coral tree in its holder",
+                value: 10,
+            },
+            {
+                description: "Flip up the coral buds",
+                value: 20,
+            },
+        ],
+    },
+    {
+        id: 2,
+        name: "Angler Fish",
+        color: "olive",
+        points: [
+            {
+                description: "Latch the angler fish within the shipwreck",
+                value: 30,
+            },
+        ],
+    },
+    {
+        id: 3,
+        name: "Raise the Mast",
+        color: "olive",
+        points: [
+            {
+                description: "Fully raise the shipwreck’s mast (mast latch engaged)",
+                value: 30,
+            },
+        ],
+    },
+    {
+        id: 4,
+        name: "Artificial Habitat",
+        color: "olive",
+        points: [
+            {
+                description: "Flatten and upright each artificial habitat stack segment",
+                value: 10,
+                max: 40,
+            },
+        ],
+    },
+    {
+        id: 5,
+        name: "Unexpected Encounter",
+        color: "olive",
+        points: [
+            {
+                description: "Release the unknown creature",
+                value: 20,
+            },
+            {
+                description: "Place the creature at least partly in the cold seep",
+                value: 10,
+            },
+        ],
+    },
+    {
+        id: 6,
+        name: "Sonar Discovery",
+        color: "olive",
+        points: [
+            {
+                description: "Reveal one whale",
+                value: 20,
+            },
+            {
+                description: "Bonus: Reveal both whales",
+                value: 30,
+            },
+        ],
+    },
+    {
+        id: 7,
+        name: "Send Over the Submersible",
+        color: "olive",
+        points: [
+            {
+                description: "Lower your team's yellow flag",
+                value: 30,
+            },
+            {
+                description: "Move the submersible closer to the opposing field",
+                value: 10,
+            },
+        ],
+    },
+    {
+        id: 8,
+        name: "Feed the Whale",
+        color: "olive",
+        points: [
+            {
+                description: "Place krill at least partly in the whale's mouth",
+                value: 10,
+                max: 30,
+            },
+        ],
+    },
+    {
+        id: 9,
+        name: "Change Shipping Lanes",
+        color: "olive",
+        points: [
+            {
+                description: "Move the ship into the new shipping lane (touching the mat)",
+                value: 20,
+            },
+        ],
+    },
+    {
+        id: 10,
+        name: "Sample Collection",
+        color: "olive",
+        points: [
+            {
+                description: "Water sample outside the water sample area",
+                value: 5,
+            },
+            {
+                description: "Seabed sample not touching the seabed",
+                value: 10,
+            },
+            {
+                description: "Plankton sample not touching the kelp forest",
+                value: 10,
+            },
+            {
+                description: "Trident part(s) no longer touching the shipwreck",
+                value: 20,
+                max: 40,
+            },
+        ],
+    },
+    {
+        id: 11,
+        name: "Research Vessel",
+        color: "olive",
+        points: [
+            {
+                description: "Place items in the research vessel's cargo area",
+                value: 5,
+            },
+            {
+                description: "Engage the port's latch in the research vessel's loop",
+                value: 20,
+            },
+        ],
+    },
+    {
+        id: 12,
+        name: "Precision Tokens",
+        color: "olive",
+        points: [
+            {
+                description: "1 token",
+                value: 10,
+            },
+            {
+                description: "2 tokens",
+                value: 15,
+            },
+            {
+                description: "3 tokens",
+                value: 25,
+            },
+            {
+                description: "4 tokens",
+                value: 35,
+            },
+            {
+                description: "5 or 6 tokens",
+                value: 50,
+            },
+        ],
+    },
+];
